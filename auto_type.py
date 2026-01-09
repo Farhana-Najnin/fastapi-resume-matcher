7cad2d73c9ae1cb7ab190b6fab95735f30db1ec0
Farhana Najnin Mahi <129548085+Farhana-Najnin@users.noreply.github.com>
2026-01-10 02:51:17 +0600

    Delete auto_type.py

--- auto_type.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-from time import sleep
-sleep(3)
-pyautogui.write('hello!',interval=.25)
-pyautogui.press('enter')
-for i in range(0,3):
-    pyautogui.write('I love you , Python',interval=0.25)
-    pyautogui.press('enter') 
-    
